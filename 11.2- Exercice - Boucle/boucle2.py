8a3d206a263f6c6adc05a0cfcd2dca857fea42d8
Rae Marcelo <elv-rae.mrcl@eduge.ch>
2019-09-17 17:12:44 +0200

    Commit : Ex 11.1 / 11.2

--- 11.2- Exercice - Boucle/boucle2.py
+++ 11.2- Exercice - Boucle/boucle2.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
 # Problème : Etant donné un tableau B de "n" nombres réels, on demande de trier le tableau B avec le tri par insertion
 # Données : un tableau B de n nombre réels
 # Résultat attendu : Le tableau B trié
 
 B = [2, 6, 8, 5, 4, 12, 98, 34, 1]
+
+print("Sans tri : ", B)
+B.sort()
+print("Tri: ", B)
--- 12.2- Exercice - Listes et Tableaux/liste2.py
+++ 12.2- Exercice - Listes et Tableaux/liste2.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
 # Problème : Etant donné un tableau, afficher l'indice du tableau comportant la valeur la plus elevée.
 # Résultat attendu : Un affichage comme ceci : "La valeur maximum est :  x  et elle se trouve à l'indice [ n ][ m ]
 
 tab = [[4, 7, 3, 20, 42], [2, 4, 5, 7, 2], [23, 24, 15, 75, 23]]
+
+for e in tab:
+    print("Valeur max: ", max(e), "Elle se trouve : ", e.index(max(e)))
